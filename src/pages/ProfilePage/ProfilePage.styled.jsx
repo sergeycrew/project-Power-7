0f49e0577d46c1cc8487509c7808cfc837768a1f
caxar777 <caxar7dam@gmail.com>
2024-03-02 19:22:09 +0200

    prepare

--- src/pages/ProfilePage/ProfilePage.styled.jsx
+++ src/pages/ProfilePage/ProfilePage.styled.jsx
@@ -1,5 +1,41 @@
 import styled from 'styled-components';
 // import { Link } from 'react-router-dom';
+import { Form } from 'formik';
+
+export const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  outline: 1px solid grey;
+  border-radius: 20px;
+  padding: 10px;
+  background-color: black;
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+    font-size: larger;
+    color: #e22b2b;
+  }
+  input {
+    margin-top: 6px;
+    border-radius: 4px;
+  }
+  button {
+    width: 100px;
+    cursor: pointer;
+    background-color: aqua;
+    background-color: black;
+    color: aqua;
+    color: red;
+    border-radius: 5px;
+    border: 1px solid aqua;
+    &:hover {
+      background-color: aqua;
+      transform: scale(1.1);
+    }
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
