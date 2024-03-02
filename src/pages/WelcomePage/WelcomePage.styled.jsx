2cec38f6290c0b3fdbd047d9965b36f94bbbd089
Avelinka <131503535+Avelinka@users.noreply.github.com>
2024-03-02 03:52:57 +0200

    add redux

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -2,28 +2,30 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex;
   justify-content: center;
-  align-items: center;
-  padding-top: 500px;
+  align-items: center; */
+  padding-top: 128px;
+  background-color: #040404;
 `;
 
 export const Title = styled.h1`
   margin: 0;
-  margin-right: 10px;
-  color: #ffffff;
-  text-shadow:
+  /* margin-right: 10px; */
+  color: #efede8;
+  margin-bottom: 40px;
+  /* text-shadow:
     0 0 5px #00baff,
     0 0 10px #00baff,
     0 0 20px #00baff,
     0 0 40px #00baff,
-    0 0 80px #00baff;
+    0 0 80px #00baff; */
 `;
-export const StyledImage = styled.img`
-  width: 40px;
-  height: 40px;
-  object-fit: cover;
-`;
+// export const StyledImage = styled.img`
+//   width: 40px;
+//   height: 40px;
+//   object-fit: cover;
+// `;
 export const StyledLink = styled(Link)`
   padding: 8px 16px;
   color: #ffffff; /* White text color */
