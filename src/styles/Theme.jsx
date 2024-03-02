adadd8369506771d5e2f12d7cba3f8f6bc3f8118
Sergey Crew <sergeycrew.bs@gmail.com>
2024-03-02 15:55:57 -0800

    Components Theme deleted. USE ONE FILE - styles-Theme

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -10,5 +10,22 @@ export const theme = {
     hoverColor: '#e6533c',
     successtColor: '#3cbf61',
     errorColor: '#d80027',
-  }
+  },
+
+
+  //код нижче для прикладу
+
+
+  // radii: {
+  //   sd: '4px',
+  //   ld: '8px',
+  // },
+
+  // spasing: (value) => `${value * 4}px`,
+
+  // shadows: {
+  //   formShadow: `0px 5px 10px 2px rgba(34, 60, 80, 0.2)`,
+  //   btnShadow: `5px 5px 5px -5px rgba(255, 255, 255, 0.8) inset`,
+  //   btnPushShadow: `3px 3px 3px -3px rgba(34, 60, 80, 0.6) inset`,
+  // },
 };
