919662344234a79eb744417e3e1139e13fac2b19
caxar777 <caxar7dam@gmail.com>
2024-03-06 19:31:40 +0200

    fixes

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 axios.defaults.baseURL = 'https://backend-power-pulse-7.onrender.com/api/';
 
 
-const temptoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWU3YWY1MTQwZmI2ZjdmNDRkOGJmOGYiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzA5NjgyNTc2LCJleHAiOjE3MDk3NjUzNzZ9.FAGKmziBB561Iaz4PbfyysplteCTZZAnoZtRTMdth9E';
+const temptoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWU3YWY1MTQwZmI2ZjdmNDRkOGJmOGYiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzA5NzQyNzkzLCJleHAiOjE3MDk4MjU1OTN9.cPR4Cz8wSDqtjENdqpJxs5AlmbjMRwVqxpezsGA3HuM';
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
