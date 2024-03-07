5c6c76a1359260af58563205b817e09483e24559
caxar777 <caxar7dam@gmail.com>
2024-03-07 18:45:07 +0200

    ok

--- src/jsonFromBd/testApi.js
+++ src/jsonFromBd/testApi.js
@@ -5,19 +5,19 @@ import axios from 'axios';
 // axios.defaults.baseURL = BASE_URL;
 
 export const getExercisesCategory = async () => {
-  const res = await axios.get(`/api/exercises/`);
+  const res = await axios.get(`exercises/`);
 
   return res.data;
 };
 
 export const getAllExercises = async () => {
-  const res = await axios.get('/api/exercises/all');
+  const res = await axios.get('exercises/all');
 
   return res.data;
 };
 
 export const addWorkout = async (workout) => {
-  const res = await axios.post('/api/diary/workout', workout);
+  const res = await axios.post('diary/workout', workout);
 
   return res.data;
 };
